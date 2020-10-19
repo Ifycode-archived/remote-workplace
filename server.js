452a8b1fea38c0113c2e3fe8bb30182ee856ed78
ifycode <marychord@gmail.com>
2020-10-18 17:35:21 -0800

    Socket.io initial set up for chat

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Setting folder containing static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+io.on('connection', socket => {
+    //testing connection
+    console.log('New WS Connection...');
+
+    /*==================================
+    Setting up connection from server to client side
+    ====================================*/
+
+    //1. For emit to the single user that is connecting
+    //e.g. Welcoming current user to the platform
+    socket.emit('message', 'Welcome to Remote Workplace chat');
+
+    //2. For all users except for the one that is connecting
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    /*3. For broadcast to all users
+        
+        io.emit();
+
+    */
+
+    //Runs when user disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+});
+
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
